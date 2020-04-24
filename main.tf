acd390487f8fed59aa21231dabbc8d3e342ca134
Administrator <admin@example.com>
2020-04-24 19:19:13 +0200

    Update msk module - cloudwatch

--- main.tf
+++ main.tf
@@ -26,12 +26,17 @@ module "network" {
   source = "./modules/network"
 }
 
+module "cloudwatch" {
+  source = "./modules/cloudwatch"
+}
+
 module "msk_cluster" {
   source = "./modules/msk_cluster"
 
   cluster_name           = "msk-terraform"
   kafka_version          = "2.2.1"
   number_of_broker_nodes = 2
+  log_group              = module.cloudwatch.log_group
   client_subnets         = module.network.subnets_private
   security_groups        = module.network.security_group
 }
