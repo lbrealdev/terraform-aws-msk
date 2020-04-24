bd18454914df9af014426eaa961ee0735d37f23f
Administrator <admin@example.com>
2020-04-24 20:50:37 +0200

    Add output msk cluster (#003)

--- modules/msk_cluster/outputs.tf
+++ modules/msk_cluster/outputs.tf
@@ -1,8 +1,11 @@
 output "cluster" {
   value = concat(
     aws_msk_cluster.main.*.cluster_name,
-    aws_msk_cluster.main.*.id,
+    aws_msk_cluster.main.*.kafka_version,
     aws_msk_cluster.main.*.arn,
-    aws_msk_cluster.main.*.broker_node_group_info
+    aws_msk_cluster.main.*.number_of_broker_nodes,
+    aws_msk_cluster.main.*.broker_node_group_info,
+    aws_msk_cluster.main.*.zookeeper_connect_string,
+    aws_msk_cluster.main.*.encryption_info
   )
 }
