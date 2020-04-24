1880aa3bb054acbd5914e072ee6100c2f21db7ca
Administrator <admin@example.com>
2020-04-24 16:03:55 +0200

    Update MSK module

--- main.tf
+++ main.tf
@@ -21,3 +21,11 @@ module "data_msk" {
   cluster_name           = "MskClusterDevCaucion"
   msk_configuration_name = "MskConfigurationClusterDevCaucion"
 }
+
+module "msk_cluster" {
+  source = "./modules/msk_cluster"
+
+  cluster_name           = "msk-terraform"
+  kafka_version          = "2.2.1"
+  number_of_broker_nodes = 2
+}
