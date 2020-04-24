899f891375f2e59fb65a8c8f463be42fdebad9af
Administrator <admin@example.com>
2020-04-24 17:46:37 +0200

    Fix msk module - msk/outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "this_vpc_id" {
-  value = module.network.this_vpc_id
+  value = module.network.vpc_id
 }
 
 output "this_subnets_private" {
-  value = module.network.this_subnet_privates
+  value = module.network.subnets_private
 }
