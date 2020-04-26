7a07217df4d15ed90303e596e82fc3b460fe4d2d
Administrator <admin@example.com>
2020-04-26 04:00:37 +0200

    Test SG-default (#022)

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "test" {
   count = var.create && data.aws_vpc.main != "" ? 1 : 0
 
   vpc_id      = data.aws_vpc.main.id
-  name        = "security.group.msk"
+  name        = lookup(var.security_group[count.index], "name", null)
   name_prefix = "sg.msk-"
   description = "Managed by Terraform"
 
